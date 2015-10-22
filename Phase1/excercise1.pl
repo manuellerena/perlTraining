588cedca26862ebcbe1d7e265236a836c940e236
manuellerena <manuel.lerena@globant.com>
2015-10-22 17:40:12 -0300

    Solve bug when a is equals to 0.

--- Phase1/excercise1.pl
+++ Phase1/excercise1.pl
@@ -2,27 +2,27 @@
 use strict;
 use v5.16;
 
-
 my $a = shift(@ARGV) || 0;
 my $b = shift(@ARGV) || 0;
 my $c = shift(@ARGV) || 0;
 my @inputs = ($a, $b, $c);
 
-
 foreach (@inputs) {
     unless (($_ =~ /^[+-]?\d+$/) || ( $_ =~ /^([+-]?)(?=\d|\.\d)\d*(\.\d*)?([Ee]([+-]?\d+))?$/)){
        die "Wrong input $_";
-
     }
 }
 my $sqrt = $b * $b - 4 * $a * $c;
 
-if ($sqrt > 0) {
-    my $x1 = (-$b + sqrt($sqrt)) / 2*$a;
-    my $x2 = (-$b - sqrt($sqrt)) / 2*$a;
+if ($a > 0 && $sqrt > 0) {
+    my $x1 = (-$b + sqrt($sqrt)) / 2 * $a;
+    my $x2 = (-$b - sqrt($sqrt)) / 2 * $a;
     say "$x1 $x2";
     exit();
+} elsif($a == 0 && $b != 0) {
+    say (-$c/$b);
 } else {
     die "No real solution\n" ;
 }
+
 
