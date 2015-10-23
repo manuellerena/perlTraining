a5af9429750662c5f4a1ba60853e8a6e5228d491
manuellerena <manuel.lerena@globant.com>
2015-10-23 15:00:07 -0300

    Excercise3

---
+++ Phase1/excercise3.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;
+use v5.16;
+
+my $wordsAsk = "Please, enter space separated strings to match and replace
+    (one pair per line) \n Empty line will interrupt input and start execution";
+say "Text a sentence to be replaced";
+my $sentence = <STDIN>;
+
+say $wordsAsk;
+my $replacePair = <STDIN>;
+my %replacements;
+while ($replacePair ne "\n") {
+
+    my @words = split(' ', $replacePair);
+    if (scalar @words == 2) {
+        $replacements{@words[0]} = @words[1];
+    }
+    say $wordsAsk;
+    $replacePair = <STDIN>;
+}
+
+my $val;
+($val = $sentence) =~ s/(@{[join "|", keys %replacements]})/$replacements{$1}/g;
+say $val;
+
+exit();
